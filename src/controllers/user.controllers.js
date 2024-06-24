27d1ac1c98e6f63357d53eae6576566a6f3c23ce
anant211205 <anant211205@gmail.com>
2024-06-24 18:42:31 +0530

    fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -218,7 +218,7 @@ const refereshAccessToken = asyncHandler(async (req , res) => {
     const incomingRefreshToken = req.cookies
     .refereshToken || req.body.refereshToken
 
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
         throw new ApiError(401 , "unauthorized request")
     }
 
